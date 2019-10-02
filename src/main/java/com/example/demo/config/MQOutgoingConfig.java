a7914e3657ef234c5705f12d9f0d7ae5508ba22b
Dewald Oosthuizen <dewald.oosthuizen@momentum.co.za>
2019-10-02 13:11:28 +0200

    created a shorter path for queue configs

--- src/main/java/com/example/demo/config/MQOutgoingConfig.java
+++ src/main/java/com/example/demo/config/MQOutgoingConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * The type Mq outgoing config.
  */
 @Configuration
-@ConfigurationProperties(prefix = "spring.application.ibm.mq.queue2")
+@ConfigurationProperties(prefix = "spring.queues.queue2")
 public class MQOutgoingConfig extends MQConfig {
     // maps all the outgoing queue settings
 }
